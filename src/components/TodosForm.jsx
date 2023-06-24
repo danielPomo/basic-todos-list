00000562054f9e447cdd325260354a22986850fa
danielPomo <danielnicolaspomo@gmail.com>
2023-06-24 02:09:26 -0300

    I complete the create functionality for the todos app, and controlled the form

--- src/components/TodosForm.jsx
+++ src/components/TodosForm.jsx
@@ -1,13 +1,30 @@
+/* eslint-disable no-unused-vars */
 import { Form, Button } from "react-bootstrap";
 import TodosList from "./TodosList";
+import { useState } from "react";
 
 const TodosForm = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("")
+  };
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex gap-2" controlId="todosForm">
-          <Form.Control type="text" placeholder="Take Peggy to the vet" />
-          <Button variant="primary">Create</Button>
+          <Form.Control
+            type="text"
+            placeholder="Take Peggy to the vet"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          />
+          <Button variant="primary" type="submit">
+            Create
+          </Button>
         </Form.Group>
       </Form>
       <TodosList />

d0331b0247f4f160b8fce379fa72e32e0b112be8
danielPomo <danielnicolaspomo@gmail.com>
2023-06-24 00:22:43 -0300

    I create the background, main title and footer for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="my-5 main">
+        <section className="text-light">
+          <h1 className="text-center ">Todos List</h1>
+          <hr />
+        </section>
+      </Container>
+      <footer className="bg-dark text-center text-light py-4">
+        <p>&copy; All rights reserved</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
